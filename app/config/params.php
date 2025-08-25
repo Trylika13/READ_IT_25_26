5d610c6e44d0fccc5e30b218fa57ea7907c205b1
Trylika <jean.g.dev@gmail.com>
2025-08-25 15:24:03 +0100

    Mise en place de la structure MVC

---
+++ app/config/params.php
@@ -0,0 +1,12 @@
+<?php
+
+// Initialisation des zones dynamiques
+
+$content = '';
+$title = '';
+
+// Paramètres de connexion
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'readit_2020');
+define('DB_USER', 'root');
+define('DB_PWD', 'root');
