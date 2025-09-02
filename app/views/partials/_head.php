5e667804a2265789761085e19f705782d3955299
Trylika <jean.g.dev@gmail.com>
2025-09-02 10:10:34 +0100

    Constante PUBLIC_BASE_URL

--- app/views/partials/_head.php
+++ app/views/partials/_head.php
@@ -1,7 +1,7 @@
 <title>Readit - <?php echo $title ?></title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<base href="http://localhost/25-26/SCRIPTS%20SERVEUR/READ_IT_25_26/public/">
+<base href="<?php echo PUBLIC_BASE_URL ?>">
 <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 
 <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
--- core/init.php
+++ core/init.php
@@ -1,5 +1,6 @@
 <?php
 
 require_once '../app/config/params.php';
+require_once '../core/constante.php';
 require_once '../core/connection.php';
 require_once '../core/helpers.php';
